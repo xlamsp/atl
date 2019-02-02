be5715814a61ff2e3c6787c2647bfe8d88bcac67
xlamsp <xlamsp@gmail.com>
2019-02-02 10:27:36 -0500

    [SHREGDRV][REFACTORY] Cleanup sherg driver, removed duplicate code from shreg_read()

--- src/shreg_driver.c
+++ src/shreg_driver.c
@@ -1,12 +1,35 @@
 #include "arduino.h"
 #include "shreg_driver.h"
 
+/*******************************************************************************
+ * Local functions
+ ******************************************************************************/
+static inline uint8_t
+shiftInOneChip(uint8_t pinData, uint8_t pinClock)
+{
+  int8_t bit;
+  uint8_t value = 0;
+
+  for (bit = 7; bit >= 0; bit--) {
+    value |= (digitalRead(pinData) << bit);
+    digitalWrite(pinClock, HIGH);
+    digitalWrite(pinClock, LOW);
+  }
+
+  return value;
+}
+
+/*******************************************************************************
+ * Public functions
+ ******************************************************************************/
+
 void
 shreg_init_input (shreg_driver_t *handle)
 {
   pinMode(handle->pinLatch, OUTPUT);
   pinMode(handle->pinClock, OUTPUT);
   pinMode(handle->pinData, INPUT);
+
   digitalWrite(handle->pinLatch, LOW);
   digitalWrite(handle->pinClock, LOW);
 }
@@ -14,55 +37,12 @@ shreg_init_input (shreg_driver_t *handle)
 void
 shreg_read (shreg_driver_t *handle, uint8_t *buffer)
 {
-  uint8_t value = 0;
-
   /* Pull up the latch to lock register input pins */
   digitalWrite(handle->pinLatch, HIGH);
 
-  /* Shift in bit from pin H */
-  value |= (digitalRead(handle->pinData) << 7);
-  digitalWrite(handle->pinClock, HIGH);
-  digitalWrite(handle->pinClock, LOW);
-
-  /* Shift in bit from pin G */
-  value |= (digitalRead(handle->pinData) << 6);
-  digitalWrite(handle->pinClock, HIGH);
-  digitalWrite(handle->pinClock, LOW);
-
-  /* Shift in bit from pin F */
-  value |= (digitalRead(handle->pinData) << 5);
-  digitalWrite(handle->pinClock, HIGH);
-  digitalWrite(handle->pinClock, LOW);
-
-  /* Shift in bit from pin E */
-  value |= (digitalRead(handle->pinData) << 4);
-  digitalWrite(handle->pinClock, HIGH);
-  digitalWrite(handle->pinClock, LOW);
-
-  /* Shift in bit from pin D */
-  value |= (digitalRead(handle->pinData) << 3);
-  digitalWrite(handle->pinClock, HIGH);
-  digitalWrite(handle->pinClock, LOW);
-
-  /* Shift in bit from pin C */
-  value |= (digitalRead(handle->pinData) << 2);
-  digitalWrite(handle->pinClock, HIGH);
-  digitalWrite(handle->pinClock, LOW);
-
-  /* Shift in bit from pin B */
-  value |= (digitalRead(handle->pinData) << 1);
-  digitalWrite(handle->pinClock, HIGH);
-  digitalWrite(handle->pinClock, LOW);
-
-  /* Shift in bit from pin A */
-  value |= digitalRead(handle->pinData);
-  digitalWrite(handle->pinClock, HIGH);
-  digitalWrite(handle->pinClock, LOW);
+  buffer[0] = shiftInOneChip(handle->pinData, handle->pinClock);
 
   /* Release the latch */
   digitalWrite(handle->pinLatch, LOW);
-
-  /* Return result */
-  buffer[0] = value;
 }
 
